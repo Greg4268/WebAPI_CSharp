51738d2ce70b24528746533cbccbe77291c9fca1
Greg <135546587+Greg4268@users.noreply.github.com>
2025-05-22 20:50:50 -0500

    regenerate using controllers base

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,6 +18,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthorization();
+
+app.MapControllers();
+
 app.Run();
-
-
