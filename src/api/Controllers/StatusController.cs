1b48c8ba1789431a866963a20f18f2009af1a768
Greg <135546587+Greg4268@users.noreply.github.com>
2025-05-24 07:55:18 -0500

    update

--- src/api/Controllers/StatusController.cs
+++ src/api/Controllers/StatusController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using api.Models;
+using api.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace api.Controllers
 {
@@ -10,6 +13,24 @@ namespace api.Controllers
     [Route("api/[controller]")]
     public class StatusController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public StatusController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         
+        [HttpPost]
+        public IActionResult GetAlert(Status message) 
+        {
+            return _statusService.Create(message);
+        }
+
+        [HttpGet]
+        public IActionResult SendResponse() {
+            
+            return NotFound();
+        }
     }
 }
